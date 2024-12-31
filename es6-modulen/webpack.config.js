3e184459759b622cf39eccc944c12ec416fadd00
jungseokheo <tjrwjdgj@gmail.com>
2024-12-31 16:26:28 +0900

    refactor: 정의에 대한 설명 주서 추가

--- es6-modulen/webpack.config.js
+++ es6-modulen/webpack.config.js
@@ -4,23 +4,23 @@ var path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './js/app.js',
+  entry: './js/app.js', // 빌드를 하는 대상 파일 정의 (자바스크립트 파일 경로), 진입점
   output: {
     filename: 'main.bundle.js',
     path: path.resolve(__dirname, 'build')
-  },
+  }, // 웹팩으로 변환을 한 후 나온 결과물의 파일 경로나 정보를 정의, 결과물
   module: {
     rules: [{
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: 'babel-loader', // 웹팩이 변환 될 때, 자바스크립트 파일이 아닌 파일에 대해서 변환을 도와주는 속성 entry -> module 변환 시 개입 하는 부분
           options: {
             presets: ['@babel/preset-env']
           }
         }
       }]
-  },
+  }, 
   stats: {
     colors: true
   },
