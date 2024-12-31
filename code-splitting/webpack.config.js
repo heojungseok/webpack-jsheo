b9a81637657f903b243cc1df27626b3fee5d709e
jungseokheo <tjrwjdgj@gmail.com>
2024-12-31 17:36:44 +0900

    feat: plugins 속성 추가

--- code-splitting/webpack.config.js
+++ code-splitting/webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   mode: 'development',
@@ -7,12 +8,18 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  module: {
+  module: { // <= loader 파일 해석 관여
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [
+            { loader: MiniCssExtractPlugin.loader },
+            "css-loader"
+          ]// 오른쪽에서 왼쪽 순으로 적용 됨. (순서가 중요함)
       }
     ]
   },
+  plugins: [// 기본적인 동작에 추가적인 기능 제공, 결과물의 형태 변형 또는 부가적 속성 적용
+    new MiniCssExtractPlugin()
+  ],
 }
